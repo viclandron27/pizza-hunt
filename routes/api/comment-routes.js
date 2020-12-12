d5342c8901c90a33f320def2c250a98850bd2444
Victoria Landron <viclandron@gmail.com>
2020-12-11 21:16:02 -0500

    adding replies

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { 
+    addComment, 
+    removeComment,
+    addReply,
+    removeReply
+} = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:_id').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:_id/:commentId').delete(removeComment);
+router
+    .route('/:_id/:commentId')
+    .put(addReply)
+    .delete(removeComment);
+
+// /api/comments/<pizzaId>/<commentId>/<replyId>
+router.route('/:_id/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
